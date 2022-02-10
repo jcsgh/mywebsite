bb4b2a6d5c5f81b16139d4bce5d3f126ef1f1858
jcsgh <business.joshuac@gmail.com>
2022-02-10 14:18:05 -0600

    footer

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div>
+      <NavBar />
       <Switch>
-        <Route path="/home" component={Home}/>
+        <Route path="/home" component={Home} />
       </Switch>
+      <Footer />
     </div>
   );
 }
