2c7d6c98152fbf566f8f854ed9c2ed0cf517487e
jcsgh <business.joshuac@gmail.com>
2022-02-10 14:42:48 -0600

    new paths

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { Switch, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import About from './pages/About'
+import Projects from './pages/Projects'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
 
@@ -10,6 +12,8 @@ function App() {
       <NavBar />
       <Switch>
         <Route path="/home" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/projects" component={Projects} />
       </Switch>
       <Footer />
     </div>
